82fa9bc89e60b428843b46087080c22c22c9465e
Jakub Zapletal <zapletal.jakub@gmail.com>
2015-04-15 21:50:53 +0100

    Change logo in the shell

--- src/Console/Shell.php
+++ src/Console/Shell.php
@@ -24,12 +24,11 @@ class Shell extends BaseShell
         $version = $this->application->getVersion();
 
         return <<<EOF
-<options=bold>      ____  _           _____
-     |  _ \| |         |___  |
-     | |_) | |__  ____    / /  ___  _ __   ___
-     |  __/|  _ \|  _ \  / /  / _ \| '_ \ / _ \
-     | |   | | | | |_) |/ /__| (_) | | | |  ___|
-     |_|   |_| |_|  __//_____|\___/|_| |_|\___|    <fg=yellow;options=bold>version {$version}</fg=yellow;options=bold>
+<options=bold>      ____  _          _____
+     |  _ \| |__  _ __|__  /___  _ __   ___
+     | |_) | '_ \| '_ \ / // _ \| '_ \ / _ \
+     |  __/| | | | |_) / /| (_) | | | |  __/
+     |_|   |_| |_| .__/____\___/|_| |_|\___|    <fg=yellow;options=bold>version {$version}</fg=yellow;options=bold>
                  |_|</options=bold>
 
 At the prompt, type <comment>help</comment> for some help,
