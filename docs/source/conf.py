8a2b57b8b27a307cc2df34c7718e09430f3fb512
Jakub Zapletal <zapletal.jakub@gmail.com>
2015-04-22 22:13:06 +0100

    Rename static folders in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ import shlex
 extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['ntemplates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -145,7 +145,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['nstatic']
+html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
